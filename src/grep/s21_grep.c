9330b7a3f9e5317c892d270614deccbec942aa06
Dmitrii <bb@localhost.localdomain>
2022-11-03 19:39:07 +0300

    grep parser

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -4,12 +4,12 @@
 
 
 typedef struct {
-    int e;      // шаблон
-    int i;      // игнорирует наличие регистра
-    int v;      // выводит строки, в которых образец не встречается 
-    int c;      // выводит количество строк, в которых обнаружен образец
-    int l;      // выводит имена файлов, содержащих образец
-    int n;      // выводит строки содержащие образец с номерами
+    int e;              // шаблон
+    int i;              // игнорирует наличие регистра
+    int v;              // выводит строки, в которых образец не встречается 
+    int c;              // выводит количество строк, в которых обнаружен образец
+    int l;              // выводит имена файлов, содержащих образец
+    int n;              // выводит строки содержащие образец с номерами
 } options;
 
 void parser(int argc, char *argv[], options *opt);
@@ -17,13 +17,21 @@ void parser(int argc, char *argv[], options *opt);
 int main(int argc, char *argv[]) {
     options opt = {0};
     parser(argc, argv, &opt);
+
+
     printf("\tСчитано: e = %d; i = %d; v = %d; c = %d; l = %d; n = %d\n", opt.e, opt.i, opt.v, opt.c, opt.l, opt.n);
+    printf("\toptind = %d\n", optind);
+    for (int i = 0; i < argc; ++i) {
+        printf("\tArgument №%d: %s\n", i, argv[i]);
+    }
+
+    
     return 0;
 }
 
 void parser(int argc, char *argv[], options *opt) {
     int rez = 0;
-    while ((rez = getopt(argc, argv, "eivcln")) != -1) {
+    while ((rez = getopt(argc, argv, "e:ivcln")) != -1) {
         switch (rez) {
         case 'e':
             opt->e++;
