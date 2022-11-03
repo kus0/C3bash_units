b22ce91af4a595f7a66ca9b6d6bc40859e710431
Dmitrii <bb@localhost.localdomain>
2022-11-04 01:22:28 +0300

    e?

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <getopt.h>
-#include <pcre.h>
+#include <string.h>
+// #include <pcre.h>
+
 
 
 typedef struct {
@@ -17,6 +19,32 @@ void parser(int argc, char *argv[], options *opt);
 int main(int argc, char *argv[]) {
     options opt = {0};
     parser(argc, argv, &opt);
+    if (opt.e == 0) {
+        char *patt;
+        patt = argv[optind];
+        // ++optind;                       // переставляем указатель на первое имя файла
+        printf("\tpattern: %s\n", patt);
+        // работа с ключами по паттерну
+    } else {
+        char *pattern[opt.e];
+        int e_count = 0;
+        for (int i = 1; i < optind; ++i) {
+            const char e_flag = 'e';
+            char *pattern_temp;
+            pattern_temp = strchr(argv[i], e_flag);
+            int pattern_temp_length = strlen(pattern_temp);
+            if (pattern_temp_length == 1) {
+                pattern[e_count] = argv[i + 1];
+                ++i;
+            } else {
+                pattern[e_count] = pattern_temp + 1;
+            }
+            ++e_count;
+        }
+        for (int i = 0; i < e_count; ++i) {
+            printf("\tpattern #%d'e': %s\n", i, pattern[i]);
+        }
+    }
 
 
     printf("\tСчитано: e = %d; i = %d; v = %d; c = %d; l = %d; n = %d\n", opt.e, opt.i, opt.v, opt.c, opt.l, opt.n);
@@ -24,8 +52,7 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < argc; ++i) {
         printf("\tArgument №%d: %s\n", i, argv[i]);
     }
-
-    
+  
     return 0;
 }
 
